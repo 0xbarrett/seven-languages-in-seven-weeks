13e1bfd9c5835ba8c697722da3c0dc3bd363c7a7
Barrett Brown <barrettb@ieee.org>
2016-04-01 10:04:37 -0400

    Erlang count to ten

--- Erlang/Day1/count.erl
+++ Erlang/Day1/count.erl
@@ -1,7 +1,7 @@
 -module(count).
 -export([to_ten/0]).
 
-next_number(10) -> 10;
-next_number(N) -> next_number(N + 1).
+next_number(10) -> io:fwrite("~w ~n", [10]);
+next_number(N) -> io:fwrite("~w ~n", [N]), next_number(N + 1).
 
 to_ten() -> next_number(0).
