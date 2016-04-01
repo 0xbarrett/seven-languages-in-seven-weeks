e7de6680cc5353afecd7312c47b3f2587cf64c3c
Barrett Brown <barrettb@ieee.org>
2016-04-01 12:08:10 -0400

    Add Erlang "Number of Words" (not working)

--- Erlang/Day1/number_of_words.erl
+++ Erlang/Day1/number_of_words.erl
@@ -1,10 +1,10 @@
 -module(number_of_words).
--export(get/1).
+-export([get/1]).
 
-get(String) ->
-  [Head | Tail]
+count([], Count) -> Count;
+count([" ", " "], Count) -> Count;
+count([" ", " "| Tail], Count) -> count(Tail, Count);
+count([" " | Tail], Count) -> count(Tail, Count + 1);
+count([Head | Tail], Count) -> count(Tail, Count).
 
-.
-
-count_remaining_words(Words) ->
-  [Head | Tail] = Words.
+get(String) -> count(String, 0).
